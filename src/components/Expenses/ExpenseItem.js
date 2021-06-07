b5a6bd49db3df8587dcabff43768dff2fab2c5c2
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2021-06-07 12:38:26 +0300

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem çalıştırıldı by React");
+
+  const clickHandler = () => {
+    setTitle("Güncellendi!");
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}TL</div>
+        <button onClick={clickHandler}>Tıkla</button>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,35 @@
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  //   const titleChangeHandler = () => {
+  //     console.log("title changed");
+  //   };
+
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler}></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01"></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2020-01-01" max="2022-12-31"></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="sumbit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
